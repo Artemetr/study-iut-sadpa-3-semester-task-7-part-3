a201ea5cb03f733417a6c7c884f6f7d6c1c1a0c5
artemetr <artem@shirokikh.ru>
2020-02-09 17:12:52 +0500

    First commit

---
+++ StudentsListForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace study_iut_sadpa_3_semester_task_7_part_3
+{
+    partial class StudentsListForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StudentsDataGridView = new System.Windows.Forms.DataGridView();
+            this.SaveStudentsDatasetToCsvButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.secondNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StudentsDataGridView
+            // 
+            this.StudentsDataGridView.AutoGenerateColumns = false;
+            this.StudentsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.groupIdDataGridViewTextBoxColumn,
+            this.lastNameDataGridViewTextBoxColumn,
+            this.firstNameDataGridViewTextBoxColumn,
+            this.secondNameDataGridViewTextBoxColumn});
+            this.StudentsDataGridView.DataSource = this.studentBindingSource;
+            this.StudentsDataGridView.Location = new System.Drawing.Point(13, 13);
+            this.StudentsDataGridView.Name = "StudentsDataGridView";
+            this.StudentsDataGridView.Size = new System.Drawing.Size(775, 396);
+            this.StudentsDataGridView.TabIndex = 0;
+            // 
+            // SaveStudentsDatasetToCsvButton
+            // 
+            this.SaveStudentsDatasetToCsvButton.Location = new System.Drawing.Point(632, 415);
+            this.SaveStudentsDatasetToCsvButton.Name = "SaveStudentsDatasetToCsvButton";
+            this.SaveStudentsDatasetToCsvButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveStudentsDatasetToCsvButton.TabIndex = 1;
+            this.SaveStudentsDatasetToCsvButton.Text = "Save";
+            this.SaveStudentsDatasetToCsvButton.UseVisualStyleBackColor = true;
+            this.SaveStudentsDatasetToCsvButton.Click += new System.EventHandler(this.SaveStudentsDatasetToCsvButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(713, 415);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Location = new System.Drawing.Point(442, 417);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.Size = new System.Drawing.Size(184, 20);
+            this.PathTextBox.TabIndex = 3;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // groupIdDataGridViewTextBoxColumn
+            // 
+            this.groupIdDataGridViewTextBoxColumn.DataPropertyName = "GroupId";
+            this.groupIdDataGridViewTextBoxColumn.HeaderText = "GroupId";
+            this.groupIdDataGridViewTextBoxColumn.Name = "groupIdDataGridViewTextBoxColumn";
+            // 
+            // lastNameDataGridViewTextBoxColumn
+            // 
+            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            // 
+            // firstNameDataGridViewTextBoxColumn
+            // 
+            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            // 
+            // secondNameDataGridViewTextBoxColumn
+            // 
+            this.secondNameDataGridViewTextBoxColumn.DataPropertyName = "SecondName";
+            this.secondNameDataGridViewTextBoxColumn.HeaderText = "SecondName";
+            this.secondNameDataGridViewTextBoxColumn.Name = "secondNameDataGridViewTextBoxColumn";
+            // 
+            // studentBindingSource
+            // 
+            this.studentBindingSource.DataSource = typeof(study_iut_sadpa_3_semester_task_7_part_3.Student);
+            // 
+            // StudentsListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PathTextBox);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.SaveStudentsDatasetToCsvButton);
+            this.Controls.Add(this.StudentsDataGridView);
+            this.Name = "StudentsListForm";
+            this.Text = "StudentsList";
+            this.Load += new System.EventHandler(this.StudentsListForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView StudentsDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn groupIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn secondNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource studentBindingSource;
+        private System.Windows.Forms.Button SaveStudentsDatasetToCsvButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.TextBox PathTextBox;
+    }
+}
+
---
+++ StudentsListForm.cs
@@ -0,0 +1,66 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace study_iut_sadpa_3_semester_task_7_part_3
+{
+    public partial class StudentsListForm : Form
+    {
+        private string PathToCsvFile { get; set; }
+
+        private bool IsStudentsDatasetSave { get; set; } = false;
+
+        public StudentsListForm(string pathToCsvFile)
+        {
+            this.PathToCsvFile = pathToCsvFile;
+            InitializeComponent();
+        }
+
+        private void SaveStudentsDatasetToCsvButton_Click(object sender, EventArgs e)
+        {
+            using(SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "CSV Files (*.csv)|*.csv", ValidateNames = true })
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName))
+                    {
+                        using (CsvWriter csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                        {
+                            csvWriter.Configuration.Delimiter = ";";
+                            csvWriter.WriteRecords(studentBindingSource.DataSource as List<Student>);
+                            //MessageBox.Show("Students data has been saved!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void StudentsListForm_Load(object sender, EventArgs e)
+        {
+            studentBindingSource.DataSource = StudentsListForm.LoadStudentsDataset(this.PathToCsvFile);
+        }
+
+        private static List<Student> LoadStudentsDataset(string pathToCsvFile)
+        {
+            List<Student> result = null;
+            using (StreamReader streamReader = new StreamReader(pathToCsvFile))
+            {
+                using (CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    csvReader.Configuration.Delimiter = ";";
+                    result = csvReader.GetRecords<Student>().ToList<Student>();
+                }
+            }
+            return result;
+        }
+    }
+}
